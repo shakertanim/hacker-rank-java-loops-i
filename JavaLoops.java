a11af5bf74e3608ed5043e7150ffc686845c4308
Kamrul Shaker <kamrul.shaker@bongobd.com>
2023-11-13 12:19:05 -0600

    Code Completed and Successful in HackerRank...

--- JavaLoops.java
+++ JavaLoops.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class JavaLoops {
     public static void main (String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        int N;
+
+        N = input.nextInt();
+        int i = 1;
+        while (i>=1 && i<=10) {
+            System.out.println(N+" x "+i+ " = "+(N*i));
+            i+=1;
+        }
     }
 }
